ec58120d1144b519e58203b22bde14e6badded57
yangxing <justlovediaodiao@outlook.com>
2023-08-03 12:22:06 +0800

    update signal

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"syscall"
 
 	"github.com/justlovediaodiao/https-proxy/client"
 )
@@ -42,7 +43,7 @@ func main() {
 	}
 
 	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, os.Interrupt, os.Kill)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	<-ch
 	client.Close()
 }
